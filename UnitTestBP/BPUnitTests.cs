5c360b372cfe47d26afb5808e5bb2940fa6b76db
Patrick Rashidi <prashidi174@gmail.com>
2025-11-20 23:30:42 +0000

    Add the first Unit Test and pass

---
+++ UnitTestBP/BPUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BPCalculator;
+
+namespace UnitTestBP;
+
+[TestClass]
+public sealed class BPUnitTests
+{
+    [TestMethod]
+    public void TestIdealPB()
+    {
+        BloodPressure bp = new BloodPressure()
+        {
+            Systolic = 110,
+            Diastolic = 70
+        };
+        Assert.AreEqual(BPCategory.Ideal, bp.Category);
+    }
+}
