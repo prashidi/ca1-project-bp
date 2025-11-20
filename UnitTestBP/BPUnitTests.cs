5e3279ec0b4b72acb4f7b455ad651d93ed254a07
Patrick Rashidi <prashidi174@gmail.com>
2025-11-20 23:44:52 +0000

    Add 9 Tests

--- UnitTestBP/BPUnitTests.cs
+++ UnitTestBP/BPUnitTests.cs
@@ -16,4 +16,28 @@ public sealed class BPUnitTests
         };
         Assert.AreEqual(BPCategory.Ideal, bp.Category);
     }
+    [DataTestMethod]
+    // Ideal
+    [DataRow(100, 70, BPCategory.Ideal)]      
+    [DataRow(110, 60, BPCategory.Ideal)]      
+    // Low
+    [DataRow(85, 55, BPCategory.Low)]
+    [DataRow(100, 45, BPCategory.Low)]
+    // Pre-High
+    [DataRow(125, 75, BPCategory.PreHigh)]
+    [DataRow(118, 85, BPCategory.PreHigh)]
+    // High
+    [DataRow(145, 70, BPCategory.High)]       
+    [DataRow(120, 95, BPCategory.High)]       
+    public void TestBPCategories(int systolic, int diastolic, BPCategory expected)
+    {
+        BloodPressure bp = new BloodPressure()
+        {
+            Systolic = systolic,
+            Diastolic = diastolic
+        };
+
+        Assert.AreEqual(expected, bp.Category);
+        
+    }
 }
